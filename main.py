a41f8f7c0d7d0ab0b283617b5091574e1acba1c9
Rohinish <singhrohinish09@gmail.com>
2024-03-04 22:54:14 +0530

    create separate endpoint for sd

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from io import BytesIO
 from PIL import Image
 import numpy as np
 import tqdm
+from stableDiffusion.pipeline import StableDiffusionPipeline
 
 app = FastAPI()
 
@@ -29,7 +30,16 @@ class ImageGenerationRequest(BaseModel):
     height: int
     guidanceScale: float
     negativePrompt: str
-    
+ 
+class StableDiffusionRequest(BaseModel):
+    model_name: str
+    seed: int
+    prompt: str
+    width: int
+    height: int
+    guidanceScale: float
+    numInferenceSteps: int
+        
 repo_id = "google/ddpm-church-256"
 
 #heartbeat
@@ -88,4 +98,26 @@ async def custom_denoising():
     image.save(image_path)
     return {"status": "success", "image_path": image_path}   
     
+
+@app.post("/stable_diffusion")
+async def stable_diffusion_pipeline(request_data: StableDiffusionRequest):
+    model_name = request_data.model_name
+    seed = request_data.seed
+    prompt = request_data.prompt
+    width = request_data.width
+    height = request_data.height
+    guidance_scale = request_data.guidanceScale
+    num_inference_steps = request_data.numInferenceSteps
     
+    sd_pipe = StableDiffusionPipeline(model_name=model_name, seed=seed)
+    
+    image = sd_pipe.generate_image([prompt], height, width, num_inference_steps, guidance_scale)
+    
+    image = (image / 2 + 0.5).clamp(0, 1).squeeze()
+    image = (image.permute(1, 2, 0) * 255).to(torch.uint8).cpu().numpy()
+    image = (image * 255).round().astype("uint8")
+    image = Image.fromarray(image)
+    
+    image_path = os.path.join(save_directory, "generated_image.png")
+    image.save(image_path)
+    return {"status": "success", "image_path": image_path}     
